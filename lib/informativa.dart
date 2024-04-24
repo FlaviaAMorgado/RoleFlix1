a8def3ef7143723c12b90f8574ff165c59f831b0
evelynkarina44 <evelynkarina535@gmail>
2024-04-24 08:38:21 -0300

    Tela informativa finalizando

--- lib/informativa.dart
+++ lib/informativa.dart
@@ -20,29 +20,85 @@ class Informativa extends StatelessWidget {
               child: ClipRRect(borderRadius: BorderRadius.circular(30),
                child: Container(
                 width: 450,
-                height: 550,
+                height: 600,
                 color: const Color.fromARGB(255, 202, 21, 8),
                child: Column (
                 mainAxisAlignment: MainAxisAlignment.start, // para alinhar 
                 children: <Widget>[
+                  const SizedBox(height: 20,), //espaço
                 Image.asset('img/poster.jpg', width: 400, height: 250, fit: BoxFit.cover,),
+                const SizedBox(height: 10,),
                 const Text(
-                  "Alice, uma menina que adormece lendo sob uma árvore, acorda em um mundo imaginativo. Ela segue um coelho branco até sua toca na floresta, onde encontra personagens falantes como um Pássaro Marinheiro, um Fantasminha Camarada, uma Porta Falante, um Bruxo atrevido e a Rainha de Copas, que traz muita confusão para a história",
+                  "Classificação: A partir 3 anos",
+                  softWrap: true,
+                  style: TextStyle(
+                    color: Color.fromARGB(226, 250, 188, 89),
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Open Sans',
+                  ),
+
+                ),
+                const SizedBox(height: 3,),
+                const Text(
+                  "Alice, uma menina que adormece lendo sob uma árvore, acorda em um mundo imaginativo. Ela segue um coelho branco até sua toca na floresta, onde encontra personagens falantes como um Pássaro Marinheiro, um Fantasminha Camarada, uma Porta Falante, um Bruxo atrevido e a Rainha de Copas, que traz muita confusão para a história.",
                   style: TextStyle(
                     color: Colors.white,
-                    fontSize: 18,
+                    fontSize: 16, 
+                    fontFamily: 'Open Sans',
+                  )
+                ),
 
-                   // textAlign: TextAlign.justify,
+                const SizedBox(height: 5,),
+                const Text(
+                  "Elenco:",
+                  softWrap: true,
+                  style: TextStyle(
+                    color: Color.fromARGB(226, 250, 188, 89),
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Open Sans',
+                  ),
+
+                ),
+                const SizedBox(height: 3,),
+                const Text(
+                  "Talytha Pugliesi, Thiago Tavares, Fani Feldman, Renan Ferraz e Álvaro Franco.",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 16, 
+                    fontFamily: 'Open Sans',
+                  )
+                ),
+
+                 const SizedBox(height: 5,),
+                const Text(
+                  "Local e Datas :",
+                  softWrap: true,
+                  style: TextStyle(
+                    color: Color.fromARGB(226, 250, 188, 89),
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Open Sans',
+                  ),
+
+                ),
+                const SizedBox(height: 3,),
+                const Text(
+                  "Teatro Augusta: De quarta a domingo, das 14h às 21h.",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 16, 
+                    fontFamily: 'Open Sans',
                   ),
                 ),
               ],
                 ),
-              )
-              )
-             
-            )
-        )
-    )
+              ),
+            ),
+            ),
+    ),
+    ),
     );
     
 
